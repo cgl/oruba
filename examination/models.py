04c2cba5a7e66f79fd6436b5b2201ad514260ed1
Çağıl Uluşahin <cagilulusahin@gmail.com>
2013-08-28 18:45:55 +0300

    initial commit

---
+++ examination/models.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+#!/usr/bin/python
+"""
+ORUBA examination Models.py
+
+examination related model classes
+
+Django models is self explanatory this code does nothing special
+
+Classes:
+
+
+"""
+__author__ =  'Cagil Ulusahin'
+__version__=  '1.0'
+
+from django.db import models
+from django.contrib.auth.models import User
+import datetime
+
+
+
